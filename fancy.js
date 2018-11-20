b9d243939fa9f2faedc73352315272bf0b0d303c
Le Duy Anh <ledzanh@gmail.com>
2018-11-20 21:52:59 +0700

    Shuffle Rs

--- fancy.js
+++ fancy.js
@@ -3,14 +3,20 @@ const axios = require('axios');
 const loading =  require('loading-cli');
 
 const accessKey = "323943-tastecli-TU53CZ9D";
-const limit = 10;
+const limit = 15;
+
+const shuffle = (anArray) => {
+  anArray = anArray.sort(() => {return 0.5 - Math.random()});
+  return anArray
+}
 
 const fetchData= (type, queryString) => {
 	const load = loading("Analyzing...").start()
 	axios.get(`https://tastedive.com/api/similar?q=${type}:${queryString}&limit=${limit}&k=${accessKey}`)
   .then(function (response) {
   	load.stop();
-    results = response.data["Similar"]["Results"];
+    results = shuffle(response.data["Similar"]["Results"]);
+    results = ((results.length > 10) ? results.slice(0, 10) : results);
     if (results.length === 0) {
     	console.log(chalk.redBright("Sorry! No results!!!"));
     }
